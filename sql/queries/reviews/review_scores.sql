5a742dda147a73cc0cf8ea1a8a0ba54e646d4d7a
Shahmeer <shahmeershahzad67@gmail.com>
2025-05-16 18:27:15 +0500

    Clean up tracked files

--- sql/queries/reviews/review_scores.sql
+++
@@ -1,7 +0,0 @@
--- Review score distribution
-SELECT 
-    review_score,
-    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM order_reviews), 2) AS percentage
-FROM order_reviews
-GROUP BY review_score
-ORDER BY review_score;
