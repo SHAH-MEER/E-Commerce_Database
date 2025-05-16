01b2bdd5ea810cc2fd2b5cce3ae209f600362e1d
Shahmeer <shahmeershahzad67@gmail.com>
2025-05-16 18:46:03 +0500

    improve script

--- sql/views/sales_overview.sql
+++ sql/views/sales_overview.sql
@@ -7,10 +7,12 @@ SELECT
     pct.product_category_name_english,
     oi.price,
     oi.freight_value,
+    o.order_status,
+    o.order_status,
     o.order_purchase_timestamp,
     DATE_TRUNC('month', o.order_purchase_timestamp) as sales_month
 FROM 
     order_items oi
-    JOIN products p ON oi.product_id = p.product_id
-    JOIN product_category_translation pct ON p.product_category_name = pct.product_category_name
-    JOIN orders o ON oi.order_id = o.order_id;
+JOIN products p ON oi.product_id = p.product_id
+JOIN product_category_translation pct ON p.product_category_name = pct.product_category_name
+JOIN orders o ON oi.order_id = o.order_id;
